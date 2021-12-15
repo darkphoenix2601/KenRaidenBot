1d58612866053922c1339bae9516fdcb9c7830c0
Wolverine <95951902+WolverinexD@users.noreply.github.com>
2021-12-15 17:38:18 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,4 +5,4 @@ class vars(object):
   API_ID = int(os.getenv("API_ID"))
   LOG_CHAT = int(os.getenv("LOG_CHAT"))
   BOT_TOKEN = os.getenv("BOT_TOKEN")
-  OWNER_ID = int(os.getenv("API_HASH"))
+  OWNER_ID = int(os.getenv("OWNER_ID"))
