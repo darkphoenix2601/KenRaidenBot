100c4935dbe75b95a78630d335094c6baa54b3bf
Wolverine <95951902+WolverinexD@users.noreply.github.com>
2021-12-15 18:36:09 +0530

    Update urlshortener.py

--- Raiden/plugins/urlshortener.py
+++ Raiden/plugins/urlshortener.py
@@ -1,7 +1,7 @@
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from Raiden import Raiden
-from Raiden.functions.shorturl import *
+from Raiden.functions import shorturl
 from Raiden.utils.url import get_url
 from Raiden.utils.errors import capture_err
 
@@ -14,6 +14,6 @@ __HELP__ = """☆ /shorturl - Short Replyed URL\n
 @capture_err
 async def shorturl(client: Client, message: Message):
   urll = get_url(message)
-  url_short = make_shorten(urll)
+  url_short = shorturl.make_shorten(urll)
   await message.reply_text("Your Short URL is\n `{url_short}`")
 # await message.reply_text("Usage : `/shorturl https://www.google.com`")
