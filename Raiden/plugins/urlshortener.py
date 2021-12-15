92721f0ba945c73fb2aa9d431f7e3918bc208add
Wolverine <95951902+WolverinexD@users.noreply.github.com>
2021-12-15 18:23:06 +0530

    Update urlshortener.py

--- Raiden/plugins/urlshortener.py
+++ Raiden/plugins/urlshortener.py
@@ -1,5 +1,5 @@
 from pyrogram import filters, Client
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from Raiden import Raiden
 from Raiden.functions.shorturl import *
 from Raiden.utils.url import get_url
